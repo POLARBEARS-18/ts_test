5a278f79ef3de263206ede82f109d2a8942e5b80
POLARBEARS-18 <r.magic.rx.8@gmail.com>
2023-10-09 23:52:59 +0900

    tsTest

--- src/tsTest/Utils/Utils.test.ts
+++ src/tsTest/Utils/Utils.test.ts
@@ -1,6 +1,48 @@
-import { toUpperCase } from './Utils'
+import { StringUtils, toUpperCase } from './Utils'
 
 describe('Utils test suite', () => {
+  describe('StringUtils tests', () => {
+    let sut: StringUtils
+
+    beforeEach(() => {
+      sut = new StringUtils()
+      console.log('Setup')
+    })
+
+    afterEach(() => {
+      // モックをクリアする
+      console.log('Teardown')
+    })
+
+    it.only('Should return correct upperCase', () => {
+      const actual = sut.toUpperCase('abc')
+
+      expect(actual).toBe('ABC')
+      console.log('Actual test')
+    })
+
+    it('Should throw error on invalid argument - function', () => {
+      const expectError = () => {
+        const actual = sut.toUpperCase('')
+      }
+      expect(expectError).toThrow()
+      expect(expectError).toThrowError('Invalid argument')
+
+      expect(() => {
+        sut.toUpperCase('')
+      }).toThrowError('Invalid argument')
+    })
+
+    it('Should throw error on invalid argument - try catch block', () => {
+      try {
+        sut.toUpperCase('')
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error)
+        expect(error).toHaveProperty('message', 'Invalid argument')
+      }
+    })
+  })
+
   it('should return uppercase', () => {
     const result = toUpperCase('abc')
     expect(result).toBe('ABC')
--- src/tsTest/Utils/Utils.ts
+++ src/tsTest/Utils/Utils.ts
@@ -1,1 +1,27 @@
+export class StringUtils {
+  // eslint-disable-next-line class-methods-use-this
+  public toUpperCase(arg: string) {
+    if (!arg) {
+      throw new Error('Invalid argument')
+    }
+    return toUpperCase(arg)
+  }
+}
+
 export const toUpperCase = (arg: string) => arg.toUpperCase()
+
+export type stringInfo = {
+  lowerCase: string
+  upperCase: string
+  characters: string[]
+  length: number
+  extraInfo: object | undefined
+}
+
+export const getStringInfo = (arg: string): stringInfo => ({
+  lowerCase: arg.toLowerCase(),
+  upperCase: arg.toUpperCase(),
+  characters: Array.from(arg),
+  length: arg.length,
+  extraInfo: {},
+})
